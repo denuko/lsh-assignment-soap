a538482d80e734b6f49ebec9aa8605c24a9fd554
Despoina Nyuu <nyuu_despoina@hotmail.com>
2018-11-04 18:32:42 +0200

    Spring initialzr

---
+++ src/main/java/lsh/assignment/soap/SoapApplication.java
@@ -0,0 +1,12 @@
+package lsh.assignment.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApplication.class, args);
+	}
+}
